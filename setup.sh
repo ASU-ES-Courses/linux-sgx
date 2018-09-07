974b825ab85d6d5079d4fba7841aea12280d9900
EugeneKuz <eugenekuznetsov@Hotmail.com>
2018-09-06 22:50:39 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,43 +1,43 @@
-echo "sudo apt-get install build-essential ocaml automake autoconf libtool wget python"
+#echo "sudo apt-get install build-essential ocaml automake autoconf libtool wget python"
 sudo apt-get install build-essential ocaml automake autoconf libtool wget python
-echo "----------------------------------------------------------------------------------------------"
+#echo "----------------------------------------------------------------------------------------------"
 
-echo "sudo apt-get install libssl-dev libcurl4-openssl-dev protobuf-compiler libprotobuf-dev"
+#echo "sudo apt-get install libssl-dev libcurl4-openssl-dev protobuf-compiler libprotobuf-dev"
 sudo apt-get install libssl-dev libcurl4-openssl-dev protobuf-compiler libprotobuf-dev
-echo "----------------------------------------------------------------------------------------------"
-echo "./download_prebuilt.sh"
+#echo "----------------------------------------------------------------------------------------------"
+#echo "./download_prebuilt.sh"
 ./download_prebuilt.sh
-echo "----------------------------------------------------------------------------------------------"
+#echo "----------------------------------------------------------------------------------------------"
 
-echo "make DEBUG=1"
+#echo "make DEBUG=1"
 make DEBUG=1
-echo "----------------------------------------------------------------------------------------------"
+#echo "----------------------------------------------------------------------------------------------"
 
-echo "make sdk_install_pkg DEBUG=1"
+#echo "make sdk_install_pkg DEBUG=1"
 make sdk_install_pkg DEBUG=1
-echo "----------------------------------------------------------------------------------------------"
+#echo "----------------------------------------------------------------------------------------------"
 
-echo "make psw_install_pkg DEBUG=1"
+#echo "make psw_install_pkg DEBUG=1"
 make psw_install_pkg DEBUG=1
-echo "----------------------------------------------------------------------------------------------"
+#echo "----------------------------------------------------------------------------------------------"
 
-echo "sudo apt-get install build-essential python"
+#echo "sudo apt-get install build-essential python"
 sudo apt-get install build-essential python
-echo "----------------------------------------------------------------------------------------------"
+#echo "----------------------------------------------------------------------------------------------"
 
-echo "./linux/installer/bin/sgx_linux_x64_sdk_2.1.3.44322.bin"
+#echo "./linux/installer/bin/sgx_linux_x64_sdk_2.1.3.44322.bin"
 ./linux/installer/bin/sgx_linux_x64_sdk_2.1.3.44322.bin
-echo "----------------------------------------------------------------------------------------------"
+#echo "----------------------------------------------------------------------------------------------"
 
-echo "./linux/installer/bin/sgx_linux_x64_sdk_2.1.3.44322.bin"
+#echo "./linux/installer/bin/sgx_linux_x64_sdk_2.1.3.44322.bin"
 source /home/eugene/SGX/linux-sgx/sgxsdk/environment
-echo "----------------------------------------------------------------------------------------------"
+#echo "----------------------------------------------------------------------------------------------"
 
-echo "sudo ./linux/installer/bin/sgx_linux_x64_psw_2.1.3.44322.bin"
+#echo "sudo ./linux/installer/bin/sgx_linux_x64_psw_2.1.3.44322.bin"
 sudo ./linux/installer/bin/sgx_linux_x64_psw_2.1.3.44322.bin
-echo "----------------------------------------------------------------------------------------------"
+#echo "----------------------------------------------------------------------------------------------"
 
-echo "chmod +x uninstall.sh"
+#echo "chmod +x uninstall.sh"
 chmod +x uninstall.sh
-echo "----------------------------------------------------------------------------------------------"
+#echo "----------------------------------------------------------------------------------------------"
 
